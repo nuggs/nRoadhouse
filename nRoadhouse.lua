50aee5c47a56e67706f786dc8737eb2e7c8451e7
Turncoat Tony <the.green.machine@gmail.com>
2015-03-23 02:18:40 -0700

    Updating and fixing

--- nRoadhouse.lua
+++ nRoadhouse.lua
@@ -1,7 +1,9 @@
-local class = select(2,UnitClass("player"));
-local kick = GetSpellInfo(1766);
-local nRoadhouse = CreateFrame("Frame");
-local player = UnitGUID("player");
+local class = select(2,UnitClass("player"))
+local kick = GetSpellInfo(1766)
+local spearhand = GetSpellInfo(116705)
+
+local nRoadhouse = CreateFrame("Frame")
+local player = UnitGUID("player")
 
 function nRoadhouse:PLAYER_LOGIN()
 	nRoadhouse:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
@@ -9,15 +11,16 @@ function nRoadhouse:PLAYER_LOGIN()
 end
 
 function nRoadhouse:COMBAT_LOG_EVENT_UNFILTERED(...)
-	local _, event, _, guid, _, _, _, destGUID, _, _, _, _, spellName = ...
-	if event == "PARTY_KILL" and guid == player and UnitIsPlayer("target") and not UnitIsTrivial("target") then
-		SendChatMessage("Road House", "SAY");
+	local _, event, _, guid, _, _, _, _, _, destFlags, _, _, spellName = ...
+
+    if event == "PARTY_KILL" and guid == player and bit.band(destFlags, COMBATLOG_OBJECT_TYPE_PLAYER) > 0 then
+		SendChatMessage("Road House", "SAY")
 	end
 
-	if event == "SPELL_INTERRUPT" and guid == UnitGUID("player") and spellName == kick and (class == "ROGUE" or class == "MONK") then
-		SendChatMessage("Round House", "SAY");
+	if event == "SPELL_INTERRUPT" and guid == UnitGUID("player") and (spellName == kick and class == "ROGUE") or (spellName == spearhand and class == "MONK") then
+		SendChatMessage("Round House", "SAY")
 	end
 end
 
-nRoadhouse:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end);
-nRoadhouse:RegisterEvent("PLAYER_LOGIN");
+nRoadhouse:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
+nRoadhouse:RegisterEvent("PLAYER_LOGIN")
