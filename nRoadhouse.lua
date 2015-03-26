dcf7de74ca7f08846d1d86cf66f699728bf711f0
Turncoat Tony <the.green.machine@gmail.com>
2015-03-25 21:46:21 -0700

    small change, it somehow broke

--- nRoadhouse.lua
+++ nRoadhouse.lua
@@ -6,21 +6,19 @@ local nRoadhouse = CreateFrame("Frame")
 local player = UnitGUID("player")
 
 function nRoadhouse:PLAYER_LOGIN()
-	nRoadhouse:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED");
 	nRoadhouse:UnregisterEvent("PLAYER_LOGIN");
 end
 
-function nRoadhouse:COMBAT_LOG_EVENT_UNFILTERED(...)
-	local _, event, _, guid, _, _, _, _, _, destFlags, _, _, spellName = ...
-
-    if event == "PARTY_KILL" and guid == player and bit.band(destFlags, COMBATLOG_OBJECT_TYPE_PLAYER) > 0 then
+function nRoadhouse:COMBAT_LOG_EVENT_UNFILTERED(_, event, _, sourceGUID, _, _, _, _, _, destFlags, _, _, spellName, ...)
+    if event == "PARTY_KILL" and sourceGUID == player and bit.band(destFlags, COMBATLOG_OBJECT_TYPE_PLAYER) > 0 then
 		SendChatMessage("Road House", "SAY")
 	end
 
-	if event == "SPELL_INTERRUPT" and guid == UnitGUID("player") and (spellName == kick and class == "ROGUE") or (spellName == spearhand and class == "MONK") then
+	if event == "SPELL_INTERRUPT" and sourceGUID == UnitGUID("player") and (spellName == kick and class == "ROGUE") or (spellName == spearhand and class == "MONK") then
 		SendChatMessage("Round House", "SAY")
 	end
 end
 
 nRoadhouse:SetScript("OnEvent", function(self, event, ...) self[event](self, ...) end)
+nRoadhouse:RegisterEvent("COMBAT_LOG_EVENT_UNFILTERED")
 nRoadhouse:RegisterEvent("PLAYER_LOGIN")
